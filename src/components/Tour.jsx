544748ed9c49d08abc1bca963c33930801d2a58e
Tarun Jangra <tarunjangra043@gmail.com>
2024-09-04 10:51:53 +0530

    push

---
+++ src/components/Tour.jsx
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+import "../styles/Tour.css";
+
+const Tour = ({ id, name, info, image, price, removeTour }) => {
+  const [readMore, setReadMore] = useState(false);
+
+  return (
+    <article className="tour">
+      <img src={image} alt={name} className="tour-img" />
+      <div className="tour-info">
+        <h4 className="tour-name">{name}</h4>
+        <h4 className="tour-price">${price}</h4>
+      </div>
+      <p className="tour-text">
+        {readMore ? info : `${info.substring(0, 200)}...`}
+        <button className="btn-read" onClick={() => setReadMore(!readMore)}>
+          {readMore ? "Show less" : "Read more"}
+        </button>
+      </p>
+      <button className="btn-delete" onClick={() => removeTour(id)}>
+        Not Interested
+      </button>
+    </article>
+  );
+};
+
+export default Tour;
---
+++ src/components/Tours.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import Tour from "./Tour";
+import "../styles/Tour.css";
+
+const Tours = ({ tours, removeTour }) => {
+  return (
+    <section>
+      <div className="title">
+        <h2>Our Tours</h2>
+        <div className="underline"></div>
+      </div>
+      <div>
+        {tours.map((tour) => (
+          <Tour key={tour.id} {...tour} removeTour={removeTour} />
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default Tours;
